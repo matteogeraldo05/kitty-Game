34bebd8a4ba19963608f678629bac49b65c9f290
Matteogeraldo05 <matteodeangelisgeraldo@gmail.com>
2023-10-24 17:23:24 -0400

    you cannot leave...

--- main.py
+++ main.py
@@ -4,34 +4,56 @@ import pygame
 from pygame import mixer
 import random
 
-def showtitleScreen():
-    tileScreen = pygame.image.load("sprites/uniChicken.png")
-    pass
+pygame.init()
+mixer.init()
+kittyIcon = pygame.image.load("favicon.ico")
+pygame.display.set_icon(kittyIcon)
+screenWidth = 360
+screenHeight = 360
+gameScreen = pygame.display.set_mode(size=(screenWidth, screenHeight)) #&Window Settings
+
+def titleScreen():
+    pygame.display.set_caption("The kitty Menu") 
+    tileImg = pygame.image.load("sprites/uniChicken.png")
+#@ THE PEN15 (EEK)! :O
+    mixer.music.load("audio/thePen15EEK.wav")
+    mixer.music.play(loops=-1)
+    mixer.music.set_volume(0.8)
+    while True:
+        gameScreen.blit(tileImg, (0, 0))
+        pygame.display.update()
+        for event in pygame.event.get():      
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    game()
+                    
+        #& Quit 
+            if event.type == pygame.QUIT: 
+                break
+    
 
 def endScreen():
-    gameOver = pygame.image.load("sprites/thisCannotContinue.png")
-    pygame.init()
     pygame.display.set_caption("The kitty Game Over :(") 
-    kittyIcon = pygame.image.load("favicon.ico")
-    pygame.display.set_icon(kittyIcon)
-
-    gameOverScreen = pygame.display.set_mode(size=(360, 360))
-    gameOverScreen.blit(gameOver, (0, 0))
+    gameOverImg = pygame.image.load("sprites/thisCannotContinue.png")
     mixer.music.stop()
     scream = mixer.Sound("audio/gameOver.wav")
     scream.play() #$ THJSI SIOS SO GOOD HAAHAWBAWHWHAHH
+    while True:
+        gameScreen.blit(gameOverImg, (0, 0))
+        pygame.display.update()
+        for event in pygame.event.get():      
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    game()
+                    
+        #& Quit 
+            if event.type == pygame.QUIT: 
+                break
 
-    pygame.display.update()
-
-def main():
-
-    pygame.init()
+def game():
     pygame.display.set_caption("The kitty Game") 
-    kittyIcon = pygame.image.load("favicon.ico")
-    pygame.display.set_icon(kittyIcon)
 
-#@ mew mew mewsic
-    mixer.init()
+#@ mew mew mewsi
     mixer.music.load("audio/MewMew.wav")
     mixer.music.play(loops=-1)
     mixer.music.set_volume(0.7)
@@ -39,14 +61,11 @@ def main():
     splat = mixer.Sound("audio/splat.wav")
     miss = mixer.Sound("audio/mwomp.wav")
     
-#&Window Settings
-    screenWidth = 360
-    screenHeight = 360
-    gameScreen = pygame.display.set_mode(size=(360, 360))
+
     
     clock = pygame.time.Clock()
 
-    #$ i am losing my grip on reality :333
+#$ i am losing my grip on reality :333
     pawImg = pygame.image.load("sprites/pawUp.png")
     hitImg = pygame.image.load("sprites/pawHit.png")
     restImg = pygame.image.load("sprites/rest.png")
@@ -62,11 +81,10 @@ def main():
     hitRect = None
     catLives = 9
 
-    def randomBug():
-        bugHeight = random.randint(250, 300)
-        print(bugHeight)
-        return bugHeight
-        
+    def randomBugSpeed():
+        bugSpeed = random.randint(3, 9)
+        #print(bugSpeed)
+        return bugSpeed
 
 #& Game loop
     notRatiod = True  
@@ -78,19 +96,18 @@ def main():
                 if event.key == pygame.K_SPACE:
                     hit = True
                     lastTick = pygame.time.get_ticks()
-                    
         #& Quit 
             if event.type == pygame.QUIT: 
-                notRatiod = False    
+                break
         
-        #? UNI ANIMATION - DONT TOUCH
+    #? UNI ANIMATION - DONT TOUCH
         nowTick = pygame.time.get_ticks()
         if hit:
             if nowTick - lastTick <= 100:
                 uniImg = pawImg
             elif 100 < nowTick - lastTick <= 450:
                 uniImg = hitImg
-                hitRect = pygame.Rect(100, 250, 20, 110)
+                hitRect = pygame.Rect(80, 260, 20, 110)
                 
             else:
                 hit = False
@@ -99,23 +116,24 @@ def main():
         else:
             uniImg = restImg
 
-        #& spawn bug
+    #& spawn bug
         if spawnBug == False:
             spawnBug = True
+            randomSpeed = randomBugSpeed()
             xPos = 500
-            bugHeight = random.randint(250, 300)
+            bugHeight = 215
         else:
             gameScreen.blit(bugImg, (xPos, bugHeight))
         
-        #& If uni misses bug
-        if xPos <= 0:
+    #& If uni misses bug
+        if xPos <= -30:
             if spawnBug == True:
                 miss.play()
                 catLives -= 1
                 spawnBug = False
             xPos = 380
         else:
-            xPos -= 6.6
+            xPos -= randomSpeed #? default is 8.8
         
         #& GameOver Check
         if catLives <= 0:
@@ -128,9 +146,11 @@ def main():
     #$ BUG YUCK!
         if spawnBug == True:
             bugRect = pygame.Rect(xPos, 315, 10, 10)
-            pygame.draw.rect(gameScreen, (1, 1, 1), bugRect)
+            gameScreen.blit(bugImg, (bugRect.x, 300))
+            #pygame.draw.rect(gameScreen, (1, 1, 1), bugRect)
 
             if hitRect is not None and hitRect.colliderect(bugRect):
+                #pygame.draw.rect(gameScreen, (255, 0, 0), hitRect) #show uni hurtbox
                 splat.play()
                 bugKillCount += 1
                 spawnBug = False
@@ -138,15 +158,19 @@ def main():
     #? UNI the CAT
         gameScreen.blit(uniImg, (0, 0))
         
+        
     #& Debug Menu Text
-        font = pygame.font.Font("freesansbold.ttf", 16)
-        killCountText = font.render(f"bugs squashed: {bugKillCount} \n lives:{catLives}", True, (255, 255, 255))
-        killCountTextRect = killCountText.get_rect()
-        gameScreen.blit(killCountText, killCountTextRect) 
+        font = pygame.font.Font("freesansbold.ttf", 20)
+        killCountText = font.render(f"bugs squashed: {bugKillCount}", True, (255, 255, 255))
+        gameScreen.blit(killCountText, (5, 5)) 
+
+        livesCountText = font.render(f"Lives:{catLives}", True, (255, 255, 255))
+        gameScreen.blit(livesCountText, (285, 5)) 
 
     #@ Update screen
         clock.tick(60)
         pygame.display.update()
 
-if __name__ == "__main__":
-    main()
+
+titleScreen()
+#game()
